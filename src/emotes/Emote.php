33b5cbf371e6c8ef3bc36d86bb7371514c4a1fd0
KosmX <kosmx.mc@gmail.com>
2022-05-15 23:02:54 +0200

    emote card, upload

--- src/emotes/Emote.php
+++ src/emotes/Emote.php
@@ -2,6 +2,8 @@
 
 namespace emotes;
 
+use elements\IElement;
+use elements\LiteralElement;
 use java\EmoteDaemonClient;
 use JetBrains\PhpStorm\ArrayShape;
 use pageUtils\UserHelper;
@@ -54,6 +56,44 @@ class Emote
             return new Emote($result->fetch_array());
         }
         return null;
+    }
+
+    public static function getSelectParams(): string
+    {
+        return " id, uuid, emoteOwner as 'ownerID', name, description, author, visibility, published ";
+    }
+
+    public function getCard(): IElement
+    {
+
+        $title = htmlspecialchars($this->name);
+        $desc = htmlspecialchars($this->description);
+        $author = htmlspecialchars($this->author);
+
+        return new LiteralElement(<<<END
+<div class="card mb-3" style="max-width: 840px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <!--If you know how to fix these image positions, please DM me!-->
+      <img src="/e/$this->id/icon" class="img-fluid rounded-start" alt="emote icon" style="object-fit: fill;">
+      <!--If you know how to fix these image positions, please DM me!-->
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">$title</h5>
+        <p class="card-text">$desc</p>
+        <p class="card-text"><small class="text-muted">$author</small></p>
+        
+        <button class="btn btn-success"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+</svg></button>
+      </div>
+    </div>
+  </div>
+</div>
+END);
+
     }
 
     /**
--- src/emotes/index.php
+++ src/emotes/index.php
@@ -6,6 +6,7 @@ use elements\AlertTag;
 use elements\IElement;
 use elements\LiteralElement;
 use elements\SimpleList;
+use java\EmoteDaemonClient;
 use pageUtils\UserHelper;
 use routing\Method;
 use routing\Router;
@@ -20,12 +21,29 @@ class index
         $R->get(Router::$EMPTY)->action(function () {return self::emoteList();});
 
         $R->all('~^new$~')->action(function () {return self::uploadEmote();});
+        $R->get('~^\\d+\\/icon$~')->action(function () {return self::getIcon();});
 
         return $R->run(getCurrentPage());
     }
 
     public static function emoteList(): ?IElement {
-        return new LiteralElement("TODO");
+        $q = getDB()->prepare("SELECT id, uuid, emoteOwner as 'ownerID', name, description, author, visibility, published FROM emotes where visibility >= 2 limit 100"); //or emoteOwner = ? and published = true
+        $q->execute();
+
+        $result = $q->get_result();
+        $emotes = array();
+        foreach ($result as $item) {
+            $emote = new Emote($item);
+            $emotes[] = $emote;
+        }
+
+        $list = new SimpleList();
+
+        foreach ($emotes as $emote) {
+            $list->addElement($emote->getCard());
+        }
+
+        return $list;
     }
 
     private static function edit()
@@ -57,6 +75,35 @@ class index
 END
         ));
         return $element;
+
+    }
+
+    private static function getIcon(): Routes
+    {
+        $id = (int)getUrlArray()[1];
+        $emote = Emote::get($id);
+        if ($emote != null && ($emote->visibility >= 1 || UserHelper::getCurrentUser() != null && $emote->ownerID == UserHelper::getCurrentUser()->userID)) {
+
+            $formatter = new EmoteDaemonClient();
+            $q = getDB()->prepare('SELECT data FROM emotes where id = ?');
+            $q->bind_param('i', $emote->id);
+            $q->execute();
+            $r = $q->get_result();
+
+            $formatter->addData($r->fetch_array()['data'], 1);
+            $r = $formatter->exchange(array(3));
+            if (strlen($r[0]['data']) != 0) {
+                header("content-type: image/png");
+                echo $r[0]['data'];
+                return Routes::SELF_SERVED;
+            }
+
+        }
+        header("content-type: image/svg+xml");
+        echo <<<END
+<svg class="bd-placeholder-img img-fluid rounded-start" width="240" height="240" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Image" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"></rect><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image</text></svg>
+END;
+        return Routes::SELF_SERVED;
 
     }
 }
