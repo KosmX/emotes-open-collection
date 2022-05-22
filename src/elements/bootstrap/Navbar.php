d9a57a290915af0f2fb72259016ff991b8a6e6a8
KosmX <kosmx.mc@gmail.com>
2022-05-22 11:17:02 +0200

    Make the search bar working

--- src/elements/bootstrap/Navbar.php
+++ src/elements/bootstrap/Navbar.php
@@ -43,8 +43,8 @@ class Navbar implements IElement
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
       $str
       </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search Emote" aria-label="Search">
+      <form class="d-flex" method="get" action="/e">
+        <input class="form-control me-2" type="search" name="s" placeholder="Search Emote" aria-label="Search">
         <button class="btn btn-outline-success" type="submit">Search</button>
       </form>
     </div>
