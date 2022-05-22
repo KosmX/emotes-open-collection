0140add7a9fb16d024e000b5302e06c6be575e5d
KosmX <kosmx.mc@gmail.com>
2022-05-23 01:28:54 +0200

    Index page

--- src/index_page.php
+++ src/index_page.php
@@ -1,12 +1,20 @@
 <?php
 
+use elements\LiteralElement;
+
 class index_page
 {
     public static function getIndex(): \elements\IElement {
 
         $l = new \elements\SimpleList();
 
-        $l->addElement(new \elements\LiteralElement("<h1>This is the main page!</h1>"));
+        $l->addElement(new LiteralElement(<<<END
+<div>
+<h1>Welcome to Emotes Open Collection!</h1>
+<hr>
+<h3>Use the navbar to search, or log-in to upload your own emotes!</h3>
+</div>
+END));
 
         return new \elements\bootstrap\Container($l, 'sm d-flex justify-content-center');
     }
