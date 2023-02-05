92bf0904821f12c34a7653cd53c81d45a24d899f
KosmX <kosmx.mc@gmail.com>
2023-02-05 12:42:25 +0100

    update title page translation code.

--- src/index_page.php
+++ src/index_page.php
@@ -1,7 +1,7 @@
-<?php declare(strict_types=1);
+<?php
 
 use elements\bootstrap\Container;
-use elements\LiteralElement;
+use i18n\TranslationInsertable;
 
 class index_page
 {
@@ -9,18 +9,51 @@ class index_page
 
         $l = new \elements\SimpleList();
 
-        $content = new \i18n\Translatable("index", array("support"=><<<END
-        <div class="col">
-        <a href="https://www.patreon.com/bePatron?u=55351945" data-patreon-widget-type="become-patron-button">Become a Patron!</a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
-        </div>
-        <div class="col">
-        <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('Support Me on Ko-fi', '#3ab372', 'G2G6505Y4');kofiwidget2.draw();</script>
-        </div>
-        END));
-        $content = $content->build();
-        $l->addElement(new LiteralElement(<<<END
+        $l->addElement(new TranslationInsertable(<<<END
 <div>
-$content
+<h1><\$index.title></h1>
+<hr>
+<h3><\$index.subtitle></h3>
+<small><\$index.title_note></small>
+<hr>
+<br><br>
+<h3><\$index.emotecraft_mod></h3>
+<h5 style="margin-bottom: 0"><a href="https://emotecraft.kosmx.dev/"><\$index.emotecraft_wiki></a> </h5>
+<small><\$index.wiki_made_by><a href="https://github.com/Kale-Ko">KaleKo</a></small>
+<br>
+<br>
+<h5><b><\$index.downloads></b></h5>
+<ul>
+<li>CurseForge
+    <ul>
+    <li><a href="https://www.curseforge.com/minecraft/mc-mods/emotecraft"><\$index.cf.for_fabric></a></li>
+    <li><a href="https://www.curseforge.com/minecraft/mc-mods/emotecraft-forge/"><\$index.cf.for_forge></a></li>
+    </ul>
+</li>
+<li><a href="https://modrinth.com/mod/emotecraft">Modrinth</a></>
+<li><a href="https://github.com/KosmX/emotes/releases">GitHub</a></>
+</ul>
+<br><br>
+<hr>
+<\$index.open_source><br>
+<a href="https://github.com/KosmX/emotes-open-collection"><i class="bi bi-github"> GitHub</i></a>
+<hr>
+<\$index.ad_free>
+<br>
+<div class="container">
+    <div class="row row-cols-auto">
+        <div class="col">
+        <a href="https://www.patreon.com/bePatron?u=55351945" data-patreon-widget-type="become-patron-button"><\$index.patreon></a><script async src="https://c6.patreon.com/becomePatronButton.bundle.js"></script>
+        </div>
+        <div class="col">
+        <script type='text/javascript' src='https://storage.ko-fi.com/cdn/widget/Widget_2.js'></script><script type='text/javascript'>kofiwidget2.init('<\$index.kofi>', '#3ab372', 'G2G6505Y4');kofiwidget2.draw();</script>
+        </div>
+        <div class="col">
+        <\$index.prefer_kofi>
+        </div>
+    </div>
+</div>
+
 </div>
 END));
 
